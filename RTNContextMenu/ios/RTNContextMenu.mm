8f1b74ae2d0d2bfe2f26b4d78a8ed322c27f7a28
aleksey-golovanov <alexey.golovanov@rokolabs.com>
2024-06-06 15:50:39 +0200

    reproduction example

---
+++ RTNContextMenu/ios/RTNContextMenu.mm
@@ -0,0 +1,58 @@
+#import "RTNContextMenu.h"
+
+#import <react/renderer/components/RTNContextMenuSpecs/ComponentDescriptors.h>
+#import <react/renderer/components/RTNContextMenuSpecs/EventEmitters.h>
+#import <react/renderer/components/RTNContextMenuSpecs/Props.h>
+#import <react/renderer/components/RTNContextMenuSpecs/RCTComponentViewHelpers.h>
+
+#import "RCTFabricComponentsPlugins.h"
+
+using namespace facebook::react;
+
+@interface RTNContextMenu () <RCTRTNContextMenuViewProtocol, UIContextMenuInteractionDelegate>
+@end
+
+@implementation RTNContextMenu {
+  UIView *_view;
+}
+
++ (ComponentDescriptorProvider)componentDescriptorProvider
+{
+  return concreteComponentDescriptorProvider<RTNContextMenuComponentDescriptor>();
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+  if (self = [super initWithFrame:frame]) {
+    static const auto defaultProps = std::make_shared<const RTNContextMenuProps>();
+    _props = defaultProps;
+
+    _view = [[UIView alloc] init];
+      
+    if (@available(iOS 13.0, *)) {
+        UIContextMenuInteraction* contextInteraction = [[UIContextMenuInteraction alloc] initWithDelegate:self];
+        [_view addInteraction:contextInteraction];
+    }
+
+    self.contentView = _view;
+  }
+    
+  return self;
+}
+
+- (nullable UIContextMenuConfiguration *)contextMenuInteraction:(nonnull UIContextMenuInteraction *)interaction configurationForMenuAtLocation:(CGPoint)location API_AVAILABLE(ios(13.0)){
+    return [UIContextMenuConfiguration configurationWithIdentifier:nil previewProvider:nil actionProvider:^UIMenu * _Nullable(NSArray<UIMenuElement *> * _Nonnull suggestedActions) {
+        UIAction *share = [UIAction actionWithTitle:@"test" image:[UIImage systemImageNamed:@"square.and.arrow.up"] identifier:nil handler:^(__kindof UIAction * _Nonnull action) {
+            // empty
+        }];
+
+        
+        return [UIMenu menuWithTitle:@"test" children:@[share]];
+    }];
+}
+@end
+
+Class<RCTComponentViewProtocol> RTNContextMenuCls(void)
+{
+  return RTNContextMenu.class;
+}
---
+++ RTNContextMenu/ios/RTNContextMenuManager.mm
@@ -0,0 +1,12 @@
+#import <React/RCTLog.h>
+#import <React/RCTUIManager.h>
+#import <React/RCTViewManager.h>
+
+@interface RTNContextMenuManager : RCTViewManager
+@end
+
+@implementation RTNContextMenuManager
+
+RCT_EXPORT_MODULE(RTNContextMenu)
+
+@end
